06be9c71660fdae9fb7e563821d2cbed298a73c1
BobLChen <chenbo@Chenbo-iMac.local>
2016-09-27 16:02:51 +0800

    add glow

---
+++ glfw/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/chenbo/Code/glfw/src/cocoa_init.m" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/chenbo/Code/glfw/src/cocoa_joystick.m" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/chenbo/Code/glfw/src/cocoa_monitor.m" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/chenbo/Code/glfw/src/cocoa_time.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/chenbo/Code/glfw/src/cocoa_window.m" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/chenbo/Code/glfw/src/context.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/chenbo/Code/glfw/src/init.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/chenbo/Code/glfw/src/input.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/chenbo/Code/glfw/src/monitor.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/chenbo/Code/glfw/src/nsgl_context.m" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/chenbo/Code/glfw/src/posix_tls.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/Users/chenbo/Code/glfw/src/vulkan.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/chenbo/Code/glfw/src/window.c" "/Users/chenbo/Code/glfw/build/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
