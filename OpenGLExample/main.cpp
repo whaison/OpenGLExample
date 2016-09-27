4f9d457278b1e99d0fad99bd029b2dc7e833e64e
BobLChen <chenbo@Chenbo-iMac.local>
2016-09-27 11:34:57 +0800

    add example

--- OpenGLExample/main.cpp
+++ OpenGLExample/main.cpp
@@ -1,12 +1,4 @@
-/**
- * OpenGL 3 - Example 01
- *
- * @author	Norbert Nopper norbert@nopper.tv
- *
- * Homepage: http://nopper.tv
- *
- * Copyright Norbert Nopper
- */
+
 
 #include <stdio.h>
 
@@ -18,8 +10,8 @@
 GLUSboolean init(GLUSvoid)
 {
     // The background will just cleared with blue color.
-    glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
-
+    glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+    
     return GLUS_TRUE;
 }
 
@@ -44,7 +36,8 @@ GLUSboolean update(GLUSfloat time)
 {
     // Now, the background is painted blue.
     glClear(GL_COLOR_BUFFER_BIT);
-
+    //glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+    
     return GLUS_TRUE;
 }
 
@@ -60,39 +53,39 @@ GLUSvoid terminate(GLUSvoid)
  */
 int main(int argc, char* argv[])
 {
-	EGLint eglConfigAttributes[] = {
-	        EGL_RED_SIZE, 8,
-	        EGL_GREEN_SIZE, 8,
-	        EGL_BLUE_SIZE, 8,
-	        EGL_DEPTH_SIZE, 0,
-	        EGL_STENCIL_SIZE, 0,
-	        EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
-	        EGL_NONE
-	};
-
-    EGLint eglContextAttributes[] = {
-    		EGL_CONTEXT_MAJOR_VERSION, 3,
-    		EGL_CONTEXT_MINOR_VERSION, 2,
-    		EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE, EGL_TRUE,
-    		EGL_CONTEXT_OPENGL_PROFILE_MASK, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT,
-    		EGL_NONE
+    EGLint eglConfigAttributes[] = {
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_DEPTH_SIZE, 0,
+        EGL_STENCIL_SIZE, 0,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+        EGL_NONE
     };
-
+    
+    EGLint eglContextAttributes[] = {
+        EGL_CONTEXT_MAJOR_VERSION, 3,
+        EGL_CONTEXT_MINOR_VERSION, 2,
+        EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE, EGL_TRUE,
+        EGL_CONTEXT_OPENGL_PROFILE_MASK, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT,
+        EGL_NONE
+    };
+    
     glusWindowSetInitFunc(init);
-
+    
     glusWindowSetReshapeFunc(reshape);
-
+    
     glusWindowSetUpdateFunc(update);
-
+    
     glusWindowSetTerminateFunc(terminate);
-
-    if (!glusWindowCreate("GLUS Example Window", 640, 480, GLUS_FALSE, GLUS_FALSE, eglConfigAttributes, eglContextAttributes, 0))
+    
+    if (!glusWindowCreate("Basic OpenGL", 800, 600, GLUS_FALSE, GLUS_FALSE, eglConfigAttributes, eglContextAttributes, 0))
     {
         printf("Could not create window!\n");
         return -1;
     }
-
+    
     glusWindowRun();
-
+    
     return 0;
 }
